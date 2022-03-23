0757b86d6c33b19d5a2c1061cfdc2881b31b3423
Jeff Rasley <jerasley@microsoft.com>
2022-03-23 15:54:18 -0700

    add reqs, pre-commits w formatting

--- README.md
+++ README.md
@@ -26,8 +26,8 @@ contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additio
 
 ## Trademarks
 
-This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft 
-trademarks or logos is subject to and must follow 
+This project may contain trademarks or logos for projects, products, or services. Authorized use of Microsoft
+trademarks or logos is subject to and must follow
 [Microsoft's Trademark & Brand Guidelines](https://www.microsoft.com/en-us/legal/intellectualproperty/trademarks/usage/general).
 Use of Microsoft trademarks or logos in modified versions of this project must not cause confusion or imply Microsoft sponsorship.
 Any use of third-party trademarks or logos are subject to those third-party's policies.
--- setup.py
+++ setup.py
@@ -4,22 +4,28 @@ Copyright 2022 The Microsoft DeepSpeed Team
 
 from setuptools import setup, find_packages
 
+
 def fetch_requirements(path):
-   with open(path, 'r') as fd:
-       return [r.strip() for r in fd.readlines()]
+    with open(path, 'r') as fd:
+        return [r.strip() for r in fd.readlines()]
 
-install_requires = fetch_requirements('requirements.txt')
 
-setup(name='mii',
-      version="0.1.0",
-      description='deepspeed mii',
-      install_requires=install_requires,
-      #scripts=[],
-      packages=find_packages(),
-      classifiers=[
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9'
-          'Programming Language :: Python :: 3.10'
-      ])
+install_requires = fetch_requirements('requirements/requirements.txt')
+
+extras_require = {"dev": fetch_requirements('requirements/requirements-dev.txt')}
+
+setup(
+    name='mii',
+    version="0.1.0",
+    description='deepspeed mii',
+    install_requires=install_requires,
+    #scripts=[],
+    extras_require=extras_require,
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+        'Programming Language :: Python :: 3.10'
+    ])
