3926d1f9de77191adf506d7d89ef634e44c96ff6
Samyam Rajbhandari <samyamr@microsoft.com>
2022-04-05 19:41:44 +0000

    Supports AML model registration. By default will download a fresh copy of the model and register it if the model has not been registered. If the aml_model_tags is provided and no previous model exists with the tag it will re-register the model. The path for checkpoints can be set using local_model_path. If no path is provided a fresh copy of the model is downloaded from default location and registered. A model can be reregistered with the same tag, and name using force_register_model. This will create a new version. The newest version of the model matching the model name and tag will be retrived for deployment

--- examples/azure-local/gpt2-azure-local-example.py
+++ examples/azure-local/gpt2-azure-local-example.py
@@ -2,11 +2,6 @@ import os
 import mii
 
 from azureml.core import Workspace
-from transformers import pipeline
-
-# TODO: if local model path not given, do this code on mii side
-os.environ["TRANSFORMERS_CACHE"] = "/tmp/mii_cache/gpt2"
-generator = pipeline('text-generation', model='gpt2')
 
 # TODO: align these environment vars w. azure naming style
 ws = Workspace(workspace_name=os.environ["AZ_WORKSPACE"],
@@ -15,7 +10,9 @@ ws = Workspace(workspace_name=os.environ["AZ_WORKSPACE"],
 
 mii.deploy(task_name="text-generation",
            model_name="gpt2",
+           aml_model_tags={'my_tag':'first_deployment'},
            deployment_type=mii.DeploymentType.AML_LOCAL,
            aml_workspace=ws,
-           aml_deployment_name="myservice",
-           local_model_path="/tmp/mii_cache/gpt2")
+           aml_deployment_name="my-gpt2-service",
+           local_model_path=None,
+           force_register_model=False)
---
+++ mii/models/utils.py
@@ -0,0 +1,37 @@
+from ctypes import wstring_at
+import os
+
+from azureml.core import Workspace
+
+import mii
+from mii.utils import mii_cache_path
+
+def supported_models_from_huggingface():
+    return ['gpt2',"deepset/roberta-large-squad2"]
+
+'''TODO make this more robust. If the pipeline has already been imported then
+this might not work since the cache is set by the first import'''
+def _download_hf_model_to_path(task, model_name, model_path):
+
+    os.environ["TRANSFORMERS_CACHE"] = model_path
+    from transformers import pipeline
+    inference_pipeline = pipeline(task, model=model_name) 
+
+
+'''generic method that will allow downloading all models that we support.
+Currently only supports HF models, but will be extended to support model checkpoints
+from other sources'''
+def download_model_and_get_path(task, model_name):
+
+    model_path = os.path.join(mii_cache_path(), model_name)
+    if not os.path.isdir(model_path):
+        os.makedirs(model_path)
+
+    if model_name in supported_models_from_huggingface():
+        _download_hf_model_to_path(task, model_name, model_path)
+    else:
+        assert False, "Only models from HF supported so far"
+
+    return model_path
+
+
