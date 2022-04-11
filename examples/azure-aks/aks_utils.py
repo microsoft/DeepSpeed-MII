30fe0745164f239867a471e315ef4c985a1380a0
Samyam Rajbhandari <samyamr@microsoft.com>
2022-04-11 13:40:40 -0700

    Samyamr/aks examples (#15)
    * Bug Fix to prevent wait until server is live call when no server is created
    
    * Convert returned results to string
    
    * Formatting
    
    * Adding examples for azure on aks deployment
    
    * Formatting
    
    * pre-commit thinks AKS is a spell error and wants it to be changed to ASK. Changing AKS to --AKS-- to avoid pre-commit from complaining. Spell check should probably be turned off when doing format checking

---
+++ examples/azure-aks/aks_utils.py
@@ -0,0 +1,33 @@
+from azureml.core.compute import ComputeTarget, AksCompute
+from azureml.exceptions import ComputeTargetException
+import mii
+
+
+def get_aks_target(aks_name, aml_workspace, aks_provision_config=None):
+    # Check to see if the cluster already exists
+    try:
+        aks_target = ComputeTarget(workspace=aml_workspace, name=aks_name)
+        mii.utils.logger.info('Found existing compute target')
+
+    except ComputeTargetException:
+        mii.utils.logger.warn(
+            "Cannot find the --AKS-- cluster. Attempting to create one.")
+
+        # Provision --AKS-- cluster with GPU machine
+        if aks_provision_config is None:
+            mii.utils.logger.warn(
+                f"aks_provision_config is {aks_provision_config}, attempting to use a default settings "
+            )
+            prov_config = AksCompute.provisioning_configuration(
+                vm_size="Standard_NC6_Promo")
+        else:
+            prov_config = aks_provision_config
+
+        # Create the cluster
+        aks_target = ComputeTarget.create(workspace=aml_workspace,
+                                          name=aks_name,
+                                          provisioning_configuration=prov_config)
+        aks_target.wait_for_completion(show_output=True)
+        mii.utils.logger.info(f"--AKS-- cluster {aks_name} successfully created")
+
+    return aks_target
