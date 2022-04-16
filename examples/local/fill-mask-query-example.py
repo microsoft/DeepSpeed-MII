8376ef40a70e8d970a87af15bfe384a2b1876a20
Cheng Li <pistasable@gmail.com>
2022-04-16 09:05:00 +0500

    add bert model type

--- examples/local/fill-mask-query-example.py
+++ examples/local/fill-mask-query-example.py
@@ -5,10 +5,13 @@ import mii
 
 # roberta
 name = "roberta-base"
-
+mask = "<mask>"
+# bert
+name = "bert-base-uncased"
+mask = "[MASK]"
 print(f"Querying {name}...")
 
 generator = mii.mii_query_handle(name + "_deployment")
-result = generator.query({'query': "Hello I'm a <mask> model."})
+result = generator.query({'query': "Hello I'm a " + mask + " model."})
 print(result.response)
 print("time_taken:", result.time_taken)
