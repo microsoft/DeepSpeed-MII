bd3ec77d3fe84bba06aca56242682eb6d8d2ee38
Michael Wyatt <michaelwyatt@microsoft.com>
2022-05-17 15:57:49 -0700

    missed some examples

--- examples/local/text-generation-example.py
+++ examples/local/text-generation-example.py
@@ -1,11 +1,8 @@
 import mii
 
-mii_configs = mii.constants.MII_CONFIGS_DEFAULT
-
 name = "distilgpt2"
 mii.deploy('text-generation',
            name,
            mii.DeploymentType.LOCAL,
            deployment_name=name + "_deployment",
-           local_model_path=".cache/models/" + name,
-           mii_configs=mii_configs)
+           local_model_path=".cache/models/" + name)
