876a9e8d34f67589b1abcfb120eaf6c1d4804ee8
Michael Wyatt <michaelwyatt@microsoft.com>
2022-05-17 16:54:02 -0700

    additional config to pydantic class

--- mii/config.py
+++ mii/config.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel
 class MIIConfig(BaseModel):
     tensor_parallel: int = 1
     port_number: int = 50050
+
+    class Config:
+        validate_all = True
+        validate_assignment = True
+        use_enum_values = True
