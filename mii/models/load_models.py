9190380a31849792d441c58fef26ee88c3be3dd5
Jeff Rasley <jerasley@microsoft.com>
2022-06-02 11:16:10 -0700

    force hf to use pytorch and not use tf

--- mii/models/load_models.py
+++ mii/models/load_models.py
@@ -11,7 +11,7 @@ def hf_provider(model_path, model_name, task_name, mii_config):
     local_rank = int(os.getenv('LOCAL_RANK', '0'))
     os.environ['TRANSFORMERS_CACHE'] = model_path
     from transformers import pipeline
-    inference_pipeline = pipeline(task_name, model=model_name, device=local_rank)
+    inference_pipeline = pipeline(task_name, model=model_name, device=local_rank, framework="pt")
     if mii_config.torch_dtype() == torch.half:
         inference_pipeline.model.half()
     return inference_pipeline
