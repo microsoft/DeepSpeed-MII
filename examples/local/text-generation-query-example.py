218d36395a8ef7dfe45e5aab0f10f1b0b69ad169
Jeff Rasley <jerasley@microsoft.com>
2022-07-23 03:10:01 +0500

    cleanup

--- examples/local/text-generation-query-example.py
+++ examples/local/text-generation-query-example.py
@@ -10,6 +10,6 @@ parser.add_argument('--deployment',
 args = parser.parse_args()
 
 generator = mii.mii_query_handle(args.deployment)
-result = generator.query({'query': ["DeepSpeed is the", "Seattle is in"]}) #, max_new_tokens=50, do_sample=True, top_p=0.9)
+result = generator.query({'query': ["DeepSpeed is the", "Seattle is"]})
 print(result.response)
 print("time_taken:", result.time_taken)
