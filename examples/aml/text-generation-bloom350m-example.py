157c22f3a08af02d91b4f6923c21a0979938de13
Ceyhun Derinbogaz <cderinbogaz@gmail.com>
2022-09-16 11:23:26 +0200

    Fixed bigscience example since 350M model is removed from Huggingface hub.

--- examples/aml/text-generation-bloom350m-example.py
+++ examples/aml/text-generation-bloom350m-example.py
@@ -3,10 +3,10 @@ import mii
 mii_configs = {
     "tensor_parallel": 1,
     "dtype": "fp16",
-    "aml_model_path": "models/bloom-350m"
+    "aml_model_path": "models/bloom-560m"
 }
 mii.deploy(task='text-generation',
-           model="bigscience/bloom-350m",
-           deployment_name="bloom350m_deployment",
+           model="bigscience/bloom-560m",
+           deployment_name="bloom560m_deployment",
            deployment_type=mii.constants.DeploymentType.AML,
            mii_config=mii_configs)
