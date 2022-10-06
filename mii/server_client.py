e1f885b55e4db930054d4ec3121acaf0d1316daf
Michael Wyatt <michaelwyatt@microsoft.com>
2022-10-06 13:20:37 -0700

    fix logic for picking provider with bloom model

--- mii/server_client.py
+++ mii/server_client.py
@@ -182,7 +182,7 @@ class MIIServerClient():
             #XXX: fetch model provider based on model name in a more general way
             if model_name == "gpt-neox":
                 provider = mii.constants.MODEL_PROVIDER_NAME_EA
-            elif "bigscience/bloom" in model_name:
+            elif ("bigscience/bloom" == model_name) or ("microsoft/bloom" in model_name):
                 provider = mii.constants.MODEL_PROVIDER_NAME_HF_LLM
             else:
                 provider = mii.constants.MODEL_PROVIDER_NAME_HF
