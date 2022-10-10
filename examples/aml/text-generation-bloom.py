43a5d41aeaf2f42f8c8a4e8beed453c5442f74de
Michael Wyatt <michaelwyatt@microsoft.com>
2022-10-10 09:55:22 -0700

    modified example to use DS-sharded checkpoint

--- examples/aml/text-generation-bloom.py
+++ examples/aml/text-generation-bloom.py
@@ -3,19 +3,11 @@ import mii
 mii_configs = {
     "dtype": "fp16",
     "tensor_parallel": 8,
-    "port_number": 50050,
-    "checkpoint_dict": {
-        "checkpoints": [f'bloom-mp_0{i}.pt' for i in range(0,
-                                                           8)],
-        "parallelization": "tp",
-        "version": 1.0,
-        "type": "BLOOM"
-    }
 }
-name = "bigscience/bloom"
+name = "microsoft/bloom-deepspeed-inference-fp16"
 
 mii.deploy(task='text-generation',
            model=name,
-           deployment_name=name + "_deployment",
+           deployment_name="bloom-deployment",
            deployment_type=mii.constants.DeploymentType.AML,
            mii_config=mii_configs)
