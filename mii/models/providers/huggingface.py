077f809f0cd07e12be64f5c7a9f62936bfe11821
l-yohai <yohan9612@naver.com>
2022-10-25 21:34:41 +0900

    use hf_auth_token in mii_config to huggingface provider

--- mii/models/providers/huggingface.py
+++ mii/models/providers/huggingface.py
@@ -8,7 +8,9 @@ def hf_provider(model_path, model_name, task_name, mii_config):
     inference_pipeline = pipeline(task_name,
                                   model=model_name,
                                   device=local_rank,
-                                  framework="pt")
+                                  framework="pt",
+                                  use_auth_token=mii_config.hf_auth_token,
+                                  )
     if mii_config.torch_dtype() == torch.half:
         inference_pipeline.model.half()
     return inference_pipeline
