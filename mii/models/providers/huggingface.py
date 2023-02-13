a5f94c66e0c3041a95c4bb2699b342c53323bc53
Michael Wyatt <michaelwyatt@microsoft.com>
2023-02-13 15:08:11 -0800

    fix device index error

--- mii/models/providers/huggingface.py
+++ mii/models/providers/huggingface.py
@@ -1,16 +1,18 @@
 import os
+import torch
 from transformers import pipeline
 
 
 def hf_provider(model_path, model_name, task_name, mii_config):
     if mii_config.load_with_sys_mem:
-        local_rank = -1
+        device = torch.device("cpu")
     else:
         local_rank = int(os.getenv('LOCAL_RANK', '0'))
+        device = torch.device(f"cuda:{local_rank}")
     inference_pipeline = pipeline(
         task_name,
         model=model_name,
-        device=local_rank,
+        device=device,
         framework="pt",
         use_auth_token=mii_config.hf_auth_token,
         torch_dtype=mii_config.dtype,
