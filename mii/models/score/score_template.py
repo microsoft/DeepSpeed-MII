a9ab1246f039f32e863cedb0cd242ae4a0c79ea7
Masahiro Tanaka <mtanaka@microsoft.com>
2023-02-22 21:55:20 +0000

    add missing parenthesis

--- mii/models/score/score_template.py
+++ mii/models/score/score_template.py
@@ -28,7 +28,8 @@ def init():
                   ds_zero=configs[mii.constants.ENABLE_DEEPSPEED_ZERO_KEY],
                   ds_config=configs[mii.constants.DEEPSPEED_CONFIG_KEY],
                   mii_configs=configs[mii.constants.MII_CONFIGS_KEY],
-                  lb_config=configs.get(mii.constants.LOAD_BALANCER_CONFIG_KEY, None)
+                  lb_config=configs.get(mii.constants.LOAD_BALANCER_CONFIG_KEY,
+                                        None))
 
     global model
     model = None
