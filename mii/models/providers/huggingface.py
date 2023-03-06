bb801d318057aeb0e4ffbb4f03cbc0fecdd4bd06
Michael Wyatt <michaelwyatt@microsoft.com>
2023-03-06 11:10:58 -0800

    allow loading of HF models with int8 dtype

--- mii/models/providers/huggingface.py
+++ mii/models/providers/huggingface.py
@@ -9,12 +9,14 @@ def hf_provider(model_path, model_name, task_name, mii_config):
     else:
         local_rank = int(os.getenv('LOCAL_RANK', '0'))
         device = torch.device(f"cuda:{local_rank}")
+    # We must load in fp16 for int8 because HF doesn't support int8
+    torch_dtype = torch.float16 if (mii_config.dtype == torch.int8) else mii_config.dtype
     inference_pipeline = pipeline(
         task_name,
         model=model_name,
         device=device,
         framework="pt",
         use_auth_token=mii_config.hf_auth_token,
-        torch_dtype=mii_config.dtype,
+        torch_dtype=torch_dtype,
     )
     return inference_pipeline
