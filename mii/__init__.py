e65eaf0b12bd3c966ad13357b178d33e9cf96007
Tosin Segun <insanechills@gmail.com>
2023-06-22 18:25:39 +0000

    removing import statement from init

--- mii/__init__.py
+++ mii/__init__.py
@@ -5,7 +5,7 @@
 import grpc
 from .server import MIIServer
 from .client import MIIClient, mii_query_handle
-from .deployment import deploy, deploy_non_persistent
+from .deployment import deploy
 from .terminate import terminate
 from .constants import DeploymentType, Tasks
 from .aml_related.utils import aml_output_path
