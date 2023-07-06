8d09b3757ba1327247979f1f8bb414bfa3ee1aa1
Tosin Segun <insanechills@gmail.com>
2023-07-06 18:27:32 +0000

    Refactored client

--- examples/multi_model/query.py
+++ examples/multi_model/query.py
@@ -6,30 +6,29 @@
 import mii
 import time
 
-generator = mii.mii_query_handle("first_test", "bloom560m_deployment")
-result = generator.query({"query": ["DeepSpeed is",
-                                    "Seattle is"]},
-                         do_sample=True,
-                         max_new_tokens=30,
-                         deployment_name="bloom560m_deployment")
+generator = mii.mii_query_handle("first_test")
+result = generator.query(
+    {"query": ["DeepSpeed is",
+               "Seattle is"]},
+    "bloom560m_deployment",
+    do_sample=True,
+    max_new_tokens=30,
+)
 print(result)
 
 time.sleep(5)
-generator2 = mii.mii_query_handle("first_test",
-                                  "microsoft/DialogRPT-human-vs-rand_deployment")
-result = generator2.query({'query': "DeepSpeed is the greatest"},
-                          deployment_name="microsoft/DialogRPT-human-vs-rand_deployment")
+result = generator.query({'query': "DeepSpeed is the greatest"},
+                         "microsoft/DialogRPT-human-vs-rand_deployment")
 print(result)
 
 time.sleep(5)
 
-generator3 = mii.mii_query_handle("first_test", "microsoft/DialoGPT-large_deployment")
-result = generator3.query(
+result = generator.query(
     {
         'text': "DeepSpeed is the greatest",
         'conversation_id': 3,
         'past_user_inputs': [],
         'generated_responses': []
     },
-    deployment_name="microsoft/DialoGPT-large_deployment")
+    "microsoft/DialoGPT-large_deployment")
 print(result)
--- mii/client.py
+++ mii/client.py
@@ -12,17 +12,28 @@ from mii.constants import GRPC_MAX_MSG_SIZE, Tasks
 from mii.method_table import GRPC_METHOD_TABLE
 
 
-def _get_deployment_info(deployment_tag, deployment_name):
+def _get_deployment_info(deployment_tag):
+    deployments = []
     configs = mii.utils.import_score_file(deployment_tag).configs
+    for deployment in configs:
+        if not isinstance(configs[deployment], dict):
+            continue
+        print("here")
+        deployments.append(configs[deployment])
+        mii_configs_dict = configs[deployment][mii.constants.MII_CONFIGS_KEY]
+        mii_configs = mii.config.MIIConfig(**mii_configs_dict)
+    return deployments
+    """
     task = configs[deployment_name][mii.constants.TASK_NAME_KEY]
     mii_configs_dict = configs[deployment_name][mii.constants.MII_CONFIGS_KEY]
     mii_configs = mii.config.MIIConfig(**mii_configs_dict)
 
     assert task is not None, "The task name should be set before calling init"
     return task, mii_configs
+    """
 
 
-def mii_query_handle(deployment_tag, deployment_name):
+def mii_query_handle(deployment_tag, deployment_name=None):
     """Get a query handle for a local deployment:
 
         mii/examples/local/gpt2-query-example.py
@@ -35,12 +46,15 @@ def mii_query_handle(deployment_tag, deployment_name):
         query_handle: A query handle with a single method `.query(request_dictionary)` using which queries can be sent to the model.
     """
 
-    if deployment_name in mii.non_persistent_models:
+    if deployment_name is not None and deployment_name in mii.non_persistent_models:
         inference_pipeline, task = mii.non_persistent_models[deployment_name]
         return MIINonPersistentClient(task, deployment_name)
 
-    task_name, mii_configs = _get_deployment_info(deployment_tag, deployment_name)
-    return MIIClient(task_name, "localhost", mii_configs.port_number)
+    deployments = _get_deployment_info(deployment_tag)
+    print(deployments)
+    mii_configs_dict = deployments[0][mii.constants.MII_CONFIGS_KEY]
+    mii_configs = mii.config.MIIConfig(**mii_configs_dict)
+    return MIIClient(deployments, "localhost", mii_configs.port_number)
 
 
 def create_channel(host, port):
@@ -55,24 +69,32 @@ class MIIClient():
     """
     Client to send queries to a single endpoint.
     """
-    def __init__(self, task_name, host, port):
+    def __init__(self, deployments, host, port):
         self.asyncio_loop = asyncio.get_event_loop()
         channel = create_channel(host, port)
         self.stub = modelresponse_pb2_grpc.ModelResponseStub(channel)
-        self.task = get_task(task_name)
+        #self.task = get_task(task_name)
+        self.deployments = deployments
 
-    async def _request_async_response(self, request_dict, **query_kwargs):
-        if self.task not in GRPC_METHOD_TABLE:
-            raise ValueError(f"unknown task: {self.task}")
+    async def _request_async_response(self, request_dict, task, **query_kwargs):
+        if task not in GRPC_METHOD_TABLE:
+            raise ValueError(f"unknown task: {task}")
 
-        task_methods = GRPC_METHOD_TABLE[self.task]
+        task_methods = GRPC_METHOD_TABLE[task]
         proto_request = task_methods.pack_request_to_proto(request_dict, **query_kwargs)
         proto_response = await getattr(self.stub, task_methods.method)(proto_request)
         return task_methods.unpack_response_from_proto(proto_response)
 
-    def query(self, request_dict, **query_kwargs):
+    def query(self, request_dict, deployment_name, **query_kwargs):
+        task = None
+        for deployment in self.deployments:
+            if deployment[mii.constants.DEPLOYMENT_NAME_KEY] == deployment_name:
+                task = get_task(deployment[mii.constants.TASK_NAME_KEY])
+                break
+        query_kwargs['deployment_name'] = deployment_name
         return self.asyncio_loop.run_until_complete(
             self._request_async_response(request_dict,
+                                         task,
                                          **query_kwargs))
 
     async def terminate_async(self):
@@ -86,8 +108,13 @@ class MIIClient():
         return await self.stub.CreateSession(
             modelresponse_pb2.SessionID(session_id=session_id))
 
-    def create_session(self, session_id):
-        assert self.task == Tasks.TEXT_GENERATION, f"Session creation only available for task '{Tasks.TEXT_GENERATION}'."
+    def create_session(self, session_id, deployment_name):
+        task = None
+        for deployment in self.deployments:
+            if deployment[mii.constants.DEPLOYMENT_NAME_KEY] == deployment_name:
+                task = get_task(deployment[mii.constants.TASK_NAME_KEY])
+                break
+        assert task == Tasks.TEXT_GENERATION, f"Session creation only available for task '{Tasks.TEXT_GENERATION}'."
         return self.asyncio_loop.run_until_complete(
             self.create_session_async(session_id))
 
@@ -95,8 +122,13 @@ class MIIClient():
         await self.stub.DestroySession(modelresponse_pb2.SessionID(session_id=session_id)
                                        )
 
-    def destroy_session(self, session_id):
-        assert self.task == Tasks.TEXT_GENERATION, f"Session deletion only available for task '{Tasks.TEXT_GENERATION}'."
+    def destroy_session(self, session_id, deployment_name):
+        task = None
+        for deployment in self.deployments:
+            if deployment[mii.constants.DEPLOYMENT_NAME_KEY] == deployment_name:
+                task = get_task(deployment[mii.constants.TASK_NAME_KEY])
+                break
+        assert task == Tasks.TEXT_GENERATION, f"Session deletion only available for task '{Tasks.TEXT_GENERATION}'."
         self.asyncio_loop.run_until_complete(self.destroy_session_async(session_id))
 
 
