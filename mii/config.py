microsoft/DeepSpeed-MII

commit 9fa7b3cf874cc566dde4d85d7442537d1c89303c

Always enable load balancing (#205)
* Removing load balancing config

* Reformatting tests

* Fixed the formatting

* Removed print statement

* Removing unused import

* Fixing tests

* Fixing merge issue

* Creating hostfile when one is not provided

* Fixing import statements removed by merge

* Removing load_balancing check

* Removing redudant definitions

* Removing hostfile from test

* Removing hostfile from non-persistent test

* fixing generated hostfile

* Updating README

mii/config.py
@@ -9,8 +9,6 @@ from pydantic import BaseModel, validator, root_validator
 
 from deepspeed.launcher.runner import DLTS_HOSTFILE
 
-from .utils import logger
-
 
 class DtypeEnum(Enum):
     # The torch dtype must always be the first value (so we return torch.dtype)
@@ -56,7 +54,6 @@ class MIIConfig(BaseModel):
     max_tokens: int = 1024
     enable_restful_api: bool = False
     restful_api_port: int = 51080
-    enable_load_balancing: bool = False
     replica_num: int = 1
     hostfile: str = DLTS_HOSTFILE
 
@@ -92,13 +89,6 @@ class MIIConfig(BaseModel):
             if not value.get(k, ''):
                 raise ValueError(f"Missing key={k} in checkpoint_dict")
         return value
-
-    @root_validator
-    def auto_enable_load_balancing(cls, values):
-        if values["enable_restful_api"] and not values["enable_load_balancing"]:
-            logger.warn("Restful API is enabled, enabling Load Balancing")
-            values["enable_load_balancing"] = True
-        return values
 
     @root_validator
     def meta_tensor_or_sys_mem(cls, values):

mii/client.py
@@ -40,13 +40,7 @@ def mii_query_handle(deployment_name):
         return MIINonPersistentClient(task, deployment_name)
 
     task_name, mii_configs = _get_deployment_info(deployment_name)
-    if mii_configs.enable_load_balancing:
-        return MIIClient(task_name, "localhost", mii_configs.port_number)
-    else:
-        return MIITensorParallelClient(
-            task_name,
-            "localhost",
-            [mii_configs.port_number + i for i in range(mii_configs.tensor_parallel)])
+    return MIIClient(task_name, "localhost", mii_configs.port_number)
 
 
 def create_channel(host, port):

mii/deployment.py
@@ -105,27 +105,25 @@ def deploy(task,
         model_path = "model"
 
     # add fields for replica deployment
-    lb_config = None
-    if mii_config.enable_load_balancing:
-        replica_pool = _allocate_processes(mii_config.hostfile,
-                                           mii_config.tensor_parallel,
-                                           mii_config.replica_num)
-        replica_configs = []
-        for i, (hostname, gpu_indices) in enumerate(replica_pool):
-            # Reserver port for a LB proxy when replication is enabled
-            port_offset = 1 if mii_config.enable_load_balancing else 0
-            base_port = mii_config.port_number + i * mii_config.tensor_parallel + port_offset
-            tensor_parallel_ports = list(
-                range(base_port,
-                      base_port + mii_config.tensor_parallel))
-            torch_dist_port = mii_config.torch_dist_port + i
-            replica_configs.append(
-                ReplicaConfig(hostname=hostname,
-                              tensor_parallel_ports=tensor_parallel_ports,
-                              torch_dist_port=torch_dist_port,
-                              gpu_indices=gpu_indices))
-        lb_config = LoadBalancerConfig(port=mii_config.port_number,
-                                       replica_configs=replica_configs)
+    replica_pool = _allocate_processes(mii_config.hostfile,
+                                       mii_config.tensor_parallel,
+                                       mii_config.replica_num)
+    replica_configs = []
+    for i, (hostname, gpu_indices) in enumerate(replica_pool):
+        # Reserver port for a LB proxy when replication is enabled
+        port_offset = 1
+        base_port = mii_config.port_number + i * mii_config.tensor_parallel + port_offset
+        tensor_parallel_ports = list(
+            range(base_port,
+                  base_port + mii_config.tensor_parallel))
+        torch_dist_port = mii_config.torch_dist_port + i
+        replica_configs.append(
+            ReplicaConfig(hostname=hostname,
+                          tensor_parallel_ports=tensor_parallel_ports,
+                          torch_dist_port=torch_dist_port,
+                          gpu_indices=gpu_indices))
+    lb_config = LoadBalancerConfig(port=mii_config.port_number,
+                                   replica_configs=replica_configs)
 
     if deployment_type != DeploymentType.NON_PERSISTENT:
         create_score_file(deployment_name=deployment_name,
@@ -144,7 +142,6 @@ def deploy(task,
     elif deployment_type == DeploymentType.LOCAL:
         return _deploy_local(deployment_name, model_path=model_path)
     elif deployment_type == DeploymentType.NON_PERSISTENT:
-        assert not mii_config.enable_load_balancing, "Cannot use Load Balancing with Non persistent deployment"
         assert int(os.getenv('WORLD_SIZE', '1')) == mii_config.tensor_parallel, "World Size does not equal number of tensors. When using non-persistent deployment type, please launch with `deepspeed --num_gpus <tensor_parallel>`"
         provider = MODEL_PROVIDER_MAP[get_provider_name(model, task)]
         mii.non_persistent_models[deployment_name] = (load_models(

mii/server.py
@@ -9,12 +9,12 @@ import subprocess
 import sys
 import tempfile
 import time
+import torch
 from pathlib import Path
 from collections import defaultdict
 
 import mii
 from mii.utils import get_num_gpus, logger, get_provider_name
-from mii.config import ReplicaConfig
 
 
 def config_to_b64_str(config):
@@ -48,9 +48,12 @@ class MIIServer():
 
         self.port_number = mii_configs.port_number
 
-        if mii_configs.enable_load_balancing and mii_configs.hostfile is None:
-            raise ValueError(
-                "hostfile must be provided if enable_load_balancing == True")
+        if mii_configs.hostfile is None:
+            hostfile = tempfile.NamedTemporaryFile(delete=False)
+            num_gpu = torch.cuda.device_count()
+            with open(hostfile, "w") as f:
+                f.write(f"localhost slots={num_gpu}")
+            mii.configs.hostfile = hostfile
 
         processes = self._initialize_service(deployment_name,
                                              model_name,
@@ -60,12 +63,7 @@ class MIIServer():
                                              ds_config,
                                              mii_configs,
                                              lb_config)
-        deployment = lb_config.replica_configs if mii_configs.enable_load_balancing else [
-            ReplicaConfig(hostname='localhost',
-                          tensor_parallel_ports=[mii_configs.port_number],
-                          torch_dist_port=mii_configs.torch_dist_port)
-        ]
-        self._wait_until_server_is_live(processes, deployment)
+        self._wait_until_server_is_live(processes, lb_config.replica_configs)
 
     def _wait_until_server_is_live(self, processes, deployment):
         for process, repl_config in zip(processes, deployment):
@@ -290,78 +288,56 @@ class MIIServer():
                             lb_config):
 
         processes = []
-        if mii_configs.enable_load_balancing:
 
-            host_gpus = defaultdict(list)
-            for repl_config in lb_config.replica_configs:
-                host_gpus[repl_config.hostname].extend(repl_config.gpu_indices)
+        host_gpus = defaultdict(list)
+        for repl_config in lb_config.replica_configs:
+            host_gpus[repl_config.hostname].extend(repl_config.gpu_indices)
 
-            # Start replica instances
-            for i, repl_config in enumerate(lb_config.replica_configs):
-                hostfile = tempfile.NamedTemporaryFile(delete=False)
-                hostfile.write(
-                    f'{repl_config.hostname} slots={max(host_gpus[repl_config.hostname])+1}\n'
-                    .encode())
-                processes.append(
-                    self._launch_deepspeed(
-                        deployment_name,
-                        model_name,
-                        model_path,
-                        ds_optimize,
-                        ds_zero,
-                        ds_config,
-                        mii_configs,
-                        hostfile.name,
-                        repl_config.hostname,
-                        repl_config.tensor_parallel_ports[0],
-                        mii_configs.torch_dist_port + (100 * i) +
-                        repl_config.gpu_indices[0],
-                        repl_config.gpu_indices))
+        # Start replica instances
+        for i, repl_config in enumerate(lb_config.replica_configs):
+            hostfile = tempfile.NamedTemporaryFile(delete=False)
+            hostfile.write(
+                f'{repl_config.hostname} slots={max(host_gpus[repl_config.hostname])+1}\n'
+                .encode())
+            processes.append(
+                self._launch_deepspeed(
+                    deployment_name,
+                    model_name,
+                    model_path,
+                    ds_optimize,
+                    ds_zero,
+                    ds_config,
+                    mii_configs,
+                    hostfile.name,
+                    repl_config.hostname,
+                    repl_config.tensor_parallel_ports[0],
+                    mii_configs.torch_dist_port + (100 * i) + repl_config.gpu_indices[0],
+                    repl_config.gpu_indices))
 
             # start load balancer here.
             # we don't use deepspeed launcher for the load balancer because it does not need a GPU.
             # The deepspeed launcher determines the number of processes to launch based on GPUs available on the host or CUDA_VISIBLE_DEVICES,
             # and it is expected to assign one GPU to one process.
-            processes.append(
-                self._launch_load_balancer(deployment_name,
-                                           model_name,
-                                           model_path,
-                                           ds_optimize,
-                                           ds_zero,
-                                           ds_config,
-                                           mii_configs,
-                                           lb_config))
-
-            if mii_configs.enable_restful_api:
-                # start rest api server
-                processes.append(
-                    self._launch_restful_gateway(deployment_name,
-                                                 model_name,
-                                                 model_path,
-                                                 ds_optimize,
-                                                 ds_zero,
-                                                 ds_config,
-                                                 mii_configs,
-                                                 mii_configs.port_number))
-
-            return processes
-        else:
-            if self._is_socket_open("localhost", self.port_number):
-                raise RuntimeError(
-                    f"Server is already running on port {self.port_number}, please shutdown or use different port."
-                )
-
-            processes.append(
-                self._launch_deepspeed(deployment_name,
+        processes.append(
+            self._launch_load_balancer(deployment_name,
                                        model_name,
                                        model_path,
                                        ds_optimize,
                                        ds_zero,
                                        ds_config,
                                        mii_configs,
-                                       '/dev/null',
-                                       'localhost',
-                                       mii_configs.port_number,
-                                       mii_configs.torch_dist_port,
-                                       mii_configs.deploy_rank))
+                                       lb_config))
+
+        if mii_configs.enable_restful_api:
+            # start rest api server
+            processes.append(
+                self._launch_restful_gateway(deployment_name,
+                                             model_name,
+                                             model_path,
+                                             ds_optimize,
+                                             ds_zero,
+                                             ds_config,
+                                             mii_configs,
+                                             mii_configs.port_number))
+
         return processes

tests/test_local_deployment.py
@@ -39,28 +39,18 @@ def mii_configs(
     port_number: int,
     meta_tensor: bool,
     load_with_sys_mem: bool,
-    enable_load_balancing: bool,
     enable_restful_api: bool,
     restful_api_port: int,
 ):
 
-    # Create a hostfile for DeepSpeed launcher when load_balancing is enabled
-    hostfile = os.path.join(tmpdir, "hostfile")
     num_gpu = torch.cuda.device_count()
-    enable_load_balancing = enable_load_balancing or enable_restful_api
-    if enable_load_balancing:
-        with open(hostfile, "w") as f:
-            f.write(f"localhost slots={num_gpu}")
-
     return {
         'dtype': dtype,
         'tensor_parallel': tensor_parallel,
         'port_number': port_number,
         'meta_tensor': meta_tensor,
         'load_with_sys_mem': load_with_sys_mem,
-        'enable_load_balancing': enable_load_balancing,
-        'replica_num': num_gpu * enable_load_balancing // tensor_parallel,
-        'hostfile': hostfile,
+        'replica_num': num_gpu // tensor_parallel,
         'enable_restful_api': enable_restful_api,
         'restful_api_port': restful_api_port,
     }
@@ -164,28 +154,6 @@ def local_deployment(deployment_config, expected_failure):
 def test_single_GPU(local_deployment, query):
     generator = mii.mii_query_handle(local_deployment.deployment_name)
     result = generator.query(query)
-    assert result
-
-
-@pytest.mark.local
-@pytest.mark.parametrize("enable_load_balancing", [True])
-@pytest.mark.parametrize("tensor_parallel", [1, 2])
-@pytest.mark.parametrize(
-    "task_name, model_name, query",
-    [
-        (
-            "text-generation",
-            "bigscience/bloom-560m",
-            {
-                "query": ["DeepSpeed is the greatest"]
-            },
-        ),
-    ],
-)
-def test_load_balancing(local_deployment, query):
-    generator = mii.mii_query_handle(local_deployment.deployment_name)
-    for _ in range(10):
-        result = generator.query(query)
     assert result
 
 

tests/test_non_persistent_deployment.py
@@ -4,7 +4,6 @@
 # DeepSpeed Team
 import pytest
 import os
-import torch
 from types import SimpleNamespace
 from .utils import *  # noqa: F401
 import mii
@@ -16,22 +15,12 @@ def mii_configs(
     dtype: str,
     tensor_parallel: int,
     load_with_sys_mem: bool,
-    enable_load_balancing: bool,
 ):
-
-    # Create a hostfile for DeepSpeed launcher when load_balancing is enabled
-    hostfile = os.path.join(tmpdir, "hostfile")
-    num_gpu = torch.cuda.device_count()
-    enable_load_balancing = enable_load_balancing
-    if enable_load_balancing:
-        with open(hostfile, "w") as f:
-            f.write(f"localhost slots={num_gpu}")
 
     return {
         'dtype': dtype,
         'tensor_parallel': tensor_parallel,
         'load_with_sys_mem': load_with_sys_mem,
-        'enable_load_balancing': enable_load_balancing,
     }
 
 
@@ -134,25 +123,3 @@ def test_single_GPU(non_persistent_deployment, query):
     generator = mii.mii_query_handle(non_persistent_deployment.deployment_name)
     result = generator.query(query)
     assert result
-
-
-@pytest.mark.local
-@pytest.mark.parametrize("enable_load_balancing", [True])
-@pytest.mark.parametrize("expected_failure", [AssertionError])
-@pytest.mark.parametrize("tensor_parallel", [1, 2])
-@pytest.mark.parametrize(
-    "task_name, model_name, query",
-    [
-        (
-            "text-generation",
-            "bigscience/bloom-560m",
-            {
-                "query": ["DeepSpeed is the greatest"]
-            },
-        ),
-    ],
-)
-def test_load_balancing(non_persistent_deployment, query):
-    print(f"TESTING NON_PERSISTENT_DEPLOYMENT: {non_persistent_deployment}")
-    assert "Cannot use Load Balancing with Non persistent deployment" in str(
-        non_persistent_deployment.value)

tests/utils.py
@@ -54,8 +54,3 @@ def ds_config(request):
 @pytest.fixture(scope="function", params=[None])
 def expected_failure(request):
     return request.param
-
-
-@pytest.fixture(scope="function", params=[False])
-def enable_load_balancing(request):
-    return request.param

commit 29260d2a3703d31a231e82881a789003e82b05c5

fix: make hostfile optional
appears to be a regression introduced in https://github.com/microsoft/DeepSpeed-MII/pull/205/files#diff-82c3dcc8668c222c5b06c4805127368ffea0e7e3616db672747b9163ea869d44R51

pydantic enforces that a non-null value is set for the hostfile. that
means that this check is forced to always succeed - preventing a
single-node setup from working

mii/config.py
@@ -3,7 +3,7 @@
 
 # DeepSpeed Team
 import torch
-from typing import Union, List
+from typing import Optional, Union, List
 from enum import Enum
 from pydantic import BaseModel, validator, root_validator
 
@@ -55,7 +55,7 @@ class MIIConfig(BaseModel):
     enable_restful_api: bool = False
     restful_api_port: int = 51080
     replica_num: int = 1
-    hostfile: str = DLTS_HOSTFILE
+    hostfile: Optional[str] = DLTS_HOSTFILE
     trust_remote_code: bool = False
 
     @validator("deploy_rank")