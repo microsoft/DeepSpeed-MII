fa701a91b867952380d3cba75c2e67dd9560ae75
Michael Wyatt <michaelwyatt@microsoft.com>
2023-10-03 16:44:02 -0700

    remove old HF cache code

--- mii/models/providers/huggingface.py
+++ mii/models/providers/huggingface.py
@@ -10,20 +10,9 @@ from deepspeed.inference.engine import InferenceEngine
 from deepspeed import OnDevice
 from pathlib import Path
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, AutoConfig
-from transformers.utils.hub import EntryNotFoundError
-from transformers.modeling_utils import get_checkpoint_shard_files
-from transformers.utils import WEIGHTS_NAME, WEIGHTS_INDEX_NAME
+from huggingface_hub import snapshot_download
 
 from mii.utils import mii_cache_path, is_aml
-
-try:
-    from transformers.utils import cached_path, hf_bucket_url
-
-    USE_NEW_HF_CACHE = False
-except ImportError:
-    from huggingface_hub import snapshot_download
-
-    USE_NEW_HF_CACHE = True
 
 
 class MetaTensorPipeline(object):
@@ -79,80 +68,29 @@ def _attempt_load(load_fn, model_name, cache_path, kwargs={}):
     return value
 
 
-def get_checkpoint_files(pretrained_model_name_or_path):
-    cache_dir = None
-    is_sharded = False
-    revision = None
-    local_files_only = False
-
-    filename = WEIGHTS_NAME
-    archive_file = hf_bucket_url(pretrained_model_name_or_path,
-                                 filename=filename,
-                                 revision=revision)
-
-    try:
-        resolved_archive_file = cached_path(
-            archive_file,
-            cache_dir=cache_dir,
-            local_files_only=local_files_only,
-        )
-        return [resolved_archive_file]
-
-    except (EntryNotFoundError, FileNotFoundError):
-        if filename == WEIGHTS_NAME:
-            # Maybe the checkpoint is sharded, we try to grab the index name in this case.
-            archive_file = hf_bucket_url(
-                pretrained_model_name_or_path,
-                filename=WEIGHTS_INDEX_NAME,
-                revision=revision,
-            )
-            resolved_archive_file = cached_path(
-                archive_file,
-                cache_dir=cache_dir,
-                local_files_only=local_files_only,
-            )
-            is_sharded = True
-
-    if is_sharded:
-        # resolved_archive_file becomes a list of files that point to the different checkpoint shards in this case.
-        resolved_archive_file, sharded_metadata = get_checkpoint_shard_files(
-            pretrained_model_name_or_path,
-            resolved_archive_file,
-            cache_dir=cache_dir,
-            revision=revision,
-        )
-
-        return resolved_archive_file
-
-
 def create_checkpoint_dict(model_name, model_path, checkpoint_dict):
-    if USE_NEW_HF_CACHE:
-        model_path = snapshot_download(
-            model_name,
-            cache_dir=model_path,
-            allow_patterns=[
-                "*.bin",
-                "*.json",
-                "*.pt",
-            ],
-            revision=None,
-        )
     if checkpoint_dict:
-        checkpoint_dict["base_dir"] = model_path
         return checkpoint_dict
-    elif os.path.isfile(os.path.join(model_path, "ds_inference_config.json")):
+    model_path = snapshot_download(
+        model_name,
+        cache_dir=model_path,
+        allow_patterns=[
+            "*.bin",
+            "*.json",
+            "*.pt",
+        ],
+        revision=None,
+    )
+    if os.path.isfile(os.path.join(model_path, "ds_inference_config.json")):
         with open(os.path.join(model_path, "ds_inference_config.json")) as f:
             data = json.load(f)
         data["base_dir"] = model_path
         return data
     else:
-        if USE_NEW_HF_CACHE:
-            checkpoint_files = [
-                str(entry).split("/")[-1]
-                for entry in Path(model_path).rglob("*.[bp][it][n]") if entry.is_file()
-            ]
-        else:
-            checkpoint_files = get_checkpoint_files(model_name)
+        checkpoint_files = [
+            str(entry).split("/")[-1]
+            for entry in Path(model_path).rglob("*.[bp][it][n]") if entry.is_file()
+        ]
         data = {
             "type": "DS_MODEL",
             "checkpoints": checkpoint_files,
