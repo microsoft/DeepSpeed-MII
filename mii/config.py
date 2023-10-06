7e78e02453854459ff654d4f7d92e3418fd727c3
blackmambaza <blackmambazaa@gmail.com>
2023-10-06 23:19:53 +0200

    Updated torch_dist_port to have a default value of 0

--- mii/config.py
+++ mii/config.py
@@ -18,7 +18,7 @@ from deepspeed.launcher.runner import DLTS_HOSTFILE, fetch_hostfile
 class ReplicaConfig(DeepSpeedConfigModel):
     hostname: str = ""
     tensor_parallel_ports: List[int] = []
-    torch_dist_port: int = None
+    torch_dist_port: int = 0
     gpu_indices: List[int] = []
 
 
