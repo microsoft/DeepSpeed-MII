33e6ec5d4ffcf5b413c3fdc83b4d6ff7512ff1c4
grajguru <grajguru@microsoft.com>
2023-11-07 13:28:26 +0530

    diffuser model load using model and path params

--- mii/legacy/models/providers/diffusers.py
+++ mii/legacy/models/providers/diffusers.py
@@ -5,8 +5,11 @@
 import os
 import torch
 
+from .utils import attempt_load
+from mii.config import ModelConfig
 
-def diffusers_provider(model_config):
+
+def diffusers_provider(model_config: ModelConfig):
     from diffusers import DiffusionPipeline
 
     local_rank = int(os.getenv("LOCAL_RANK", "0"))
@@ -16,7 +19,9 @@ def diffusers_provider(model_config):
         kwargs["torch_dtype"] = torch.float16
         kwargs["revision"] = "fp16"
 
-    pipeline = DiffusionPipeline.from_pretrained(model_config.model_name, **kwargs)
+    pipeline = attempt_load(DiffusionPipeline.from_pretrained,
+                            model_config.model, model_config.model_path,
+                            kwargs=kwargs)
     pipeline = pipeline.to(f"cuda:{local_rank}")
     pipeline.set_progress_bar_config(disable=True)
     return pipeline
