fea34b95a777f4ee9a162d54c4c89f50426c957b
Michael Wyatt <michaelwyatt@microsoft.com>
2023-11-21 15:46:55 -0800

    add conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'DeepSpeed-MII'
+copyright = '2023, Microsoft'
+author = 'Microsoft'
+
+release = '0.1.2'
+version = '0.1.2'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/',
+               None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/',
+               None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
