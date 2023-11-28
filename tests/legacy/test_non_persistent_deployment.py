5d3fac681f83dfb4b8de92f4d4bd2a7caf6c7ed9
Michael Wyatt <michaelwyatt@microsoft.com>
2023-11-28 14:24:54 -0800

    fix non-persistent test

--- tests/legacy/test_non_persistent_deployment.py
+++ tests/legacy/test_non_persistent_deployment.py
@@ -9,80 +9,74 @@ import mii.legacy as mii
 
 @pytest.mark.parametrize("deployment_type", [mii.DeploymentType.NON_PERSISTENT])
 @pytest.mark.parametrize(
-    "task_name, model_name, query, replace_with_kernel_inject",
-    [(
-        "conversational",
-        "microsoft/DialoGPT-small",
-        {
-            "text": "DeepSpeed is the greatest",
-            "conversation_id": 3,
-            "past_user_inputs": [],
-            "generated_responses": [],
-        },
-        True,
-    ),
-     (
-         "fill-mask",
-         "bert-base-uncased",
-         {
-             "query": "Hello I'm a [MASK] model."
-         },
-         True,
-     ),
-     (
-         "question-answering",
-         "deepset/roberta-large-squad2",
-         {
-             "question": "What is the greatest?",
-             "context": "DeepSpeed is the greatest",
-         },
-         True,
-     ),
-     (
-         "text-generation",
-         "distilgpt2",
-         {
-             "query": ["DeepSpeed is the greatest"]
-         },
-         True,
-     ),
-     (
-         "text-generation",
-         "bigscience/bloom-560m",
-         {
-             "query": ["DeepSpeed is the greatest",
-                       "Seattle is"]
-         },
-         True,
-     ),
-     (
-         "token-classification",
-         "Jean-Baptiste/roberta-large-ner-english",
-         {
-             "query": "My name is jean-baptiste and I live in montreal."
-         },
-         True,
-     ),
-     (
-         "text-classification",
-         "roberta-large-mnli",
-         {
-             "query": "DeepSpeed is the greatest"
-         },
-         True,
-     ),
-     (
-         "zero-shot-image-classification",
-         "openai/clip-vit-base-patch32",
-         {
-             "image":
-             "https://huggingface.co/datasets/Narsil/image_dummy/raw/main/parrots.png",
-             "candidate_labels": ["animals",
-                                  "humans",
-                                  "landscape"]
-         },
-         False,
-     )],
+    "task_name, model_name, query",
+    [
+        (
+            "conversational",
+            "microsoft/DialoGPT-small",
+            {
+                "text": "DeepSpeed is the greatest",
+                "conversation_id": 3,
+                "past_user_inputs": [],
+                "generated_responses": [],
+            },
+        ),
+        (
+            "fill-mask",
+            "bert-base-uncased",
+            {
+                "query": "Hello I'm a [MASK] model."
+            },
+        ),
+        (
+            "question-answering",
+            "deepset/roberta-large-squad2",
+            {
+                "question": "What is the greatest?",
+                "context": "DeepSpeed is the greatest",
+            },
+        ),
+        (
+            "text-generation",
+            "distilgpt2",
+            {
+                "query": ["DeepSpeed is the greatest"]
+            },
+        ),
+        (
+            "text-generation",
+            "bigscience/bloom-560m",
+            {
+                "query": ["DeepSpeed is the greatest",
+                          "Seattle is"]
+            },
+        ),
+        (
+            "token-classification",
+            "Jean-Baptiste/roberta-large-ner-english",
+            {
+                "query": "My name is jean-baptiste and I live in montreal."
+            },
+        ),
+        (
+            "text-classification",
+            "roberta-large-mnli",
+            {
+                "query": "DeepSpeed is the greatest"
+            },
+        ),
+        (
+            "zero-shot-image-classification",
+            "openai/clip-vit-base-patch32",
+            {
+                "image":
+                "https://huggingface.co/datasets/Narsil/image_dummy/raw/main/parrots.png",
+                "candidate_labels": ["animals",
+                                     "humans",
+                                     "landscape"],
+            },
+        ),
+    ],
 )
 def test_single_GPU(deployment, query):
     generator = mii.mii_query_handle(deployment)
