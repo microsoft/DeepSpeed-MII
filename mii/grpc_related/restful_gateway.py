5e12a38520b4cd9eb63a2790c3611c837f723885
Michael Wyatt <michaelwyatt@microsoft.com>
2023-12-05 14:06:46 -0800

    Add RESTful API option for host (#348)

--- mii/grpc_related/restful_gateway.py
+++ mii/grpc_related/restful_gateway.py
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-import threading
+import asyncio
 import time
+import threading
 
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
@@ -22,6 +23,8 @@ def createRestfulGatewayApp(deployment_name, server_thread):
     class RestfulGatewayService(Resource):
         def __init__(self):
             super().__init__()
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
             self.client = mii.client(deployment_name)
 
         def post(self):
@@ -45,11 +48,11 @@ def createRestfulGatewayApp(deployment_name, server_thread):
 
 
 class RestfulGatewayThread(threading.Thread):
-    def __init__(self, deployment_name, rest_port, rest_procs):
+    def __init__(self, deployment_name, rest_host, rest_port, rest_procs):
         threading.Thread.__init__(self)
 
         app = createRestfulGatewayApp(deployment_name, self)
-        self.server = make_server("127.0.0.1",
+        self.server = make_server(rest_host,
                                   rest_port,
                                   app,
                                   threaded=False,
